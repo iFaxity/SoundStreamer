cd1d700b406acc066ee7d77ff0b403f8b75a3c6b
iFaxity <izze96@gmail.com>
2015-09-07 21:22:41 +0200

    Added Installer to project

---
+++ Installer/Core.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using Ionic.Zip;
+
+namespace Installer {
+    class Core {
+        public static void Install(string path = "", string version = "latest") {
+            var wc = new WebClient();
+            wc.DownloadDataCompleted += (sender, e) => {
+                if(e.Cancelled || e.Error != null)
+                    return;
+
+                using(var stream = new MemoryStream(e.Result)) {
+                    using(var zip = ZipFile.Read(stream)) {
+                        zip.ExtractAll(path, ExtractExistingFileAction.OverwriteSilently);
+                    }
+                }
+            };
+            wc.DownloadDataAsync(new Uri("http://scdesktop.us.to/download.php?v=" + version));
+        }
+
+        public static bool IsFileOpen(string file) {
+            FileStream stream = null;
+            try {
+                stream = File.Open(file, FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch(IOException) {
+                return true;
+            }
+            finally {
+                if(stream != null)
+                    stream.Close();
+            }
+            return false;
+        }
+    }
+}
---
+++ Installer/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            var args = Environment.GetCommandLineArgs().ToList();
+            InitializeComponent();
+
+            foreach(var proc in Process.GetProcesses()) {
+                if(proc.ProcessName == "SoundCloud.Desktop") {
+                    var res = MessageBox.Show("Please close the 'SoundCloud Desktop' application to continue. Press 'Yes' after you have closed it.", "Application not closed", MessageBoxButton.OKCancel);
+
+                    if(res == MessageBoxResult.Cancel)
+                        Environment.Exit(0);
+                }
+            }
+
+            // Only update
+            if(args.Contains("update")) {
+                if(File.Exists("SoundCloud.Desktop.exe")) {
+                    while(Core.IsFileOpen("SoundCloud.Desktop.exe")) {
+                        MessageBox.Show("Please close the 'SoundCloud Desktop' to continue updating.", "Update prevented", MessageBoxButton.OK);
+                    }
+                    Core.Install("");
+                }
+                Environment.Exit(0);
+            }
+            // Force install
+            else if(args.Contains("force")) {
+
+            }
+            // Install dialog
+            else {
+
+            }
+        }
+    }
+}
